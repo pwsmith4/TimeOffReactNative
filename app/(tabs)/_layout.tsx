fae2721afa569c8ead62c6fe7e0eedfe87aeb146
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-06-13 16:22:06 -0700

    Time Off Details Added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
-
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import TabTwoScreen from './two';
+import TabOneScreen from '.';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -14,46 +17,41 @@ function TabBarIcon(props: {
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
-
+type RootStackParamList = {
+  TabOneScreen: undefined;
+  TimeOffDetails: undefined;
+  // other routes...
+};
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const Stack = createStackNavigator();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
+    <Stack.Navigator>
+      <Stack.Screen
+  name="index"
+  options={{
+    title: 'Tab One',
+    headerRight: () => (
+            <Pressable onPress={() => navigation.navigate('TimeOffDetails')}>
+            {({ pressed }) => (
+            <FontAwesome
+              name="info-circle"
+              size={25}
+              color={Colors[colorScheme ?? 'light'].text}
+              style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+            />
+          )}
+        </Pressable>
+    ),
+  }}
+  component={TabOneScreen} // replace with your actual component
+/>
+<Stack.Screen
+    name="TimeOffDetails"
+    component={TabTwoScreen} // replace with your actual component
+  />
+    </Stack.Navigator>
   );
 }
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,35 +1,117 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View, Dimensions, TextInput} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Text } from '@/components/Themed';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import React, { useState } from 'react';
 
 export default function ModalScreen() {
+  const [selectedDates, setSelectedDates] = useState<{start: string | null, end: string | null, selectedDates: Record<string, any>}>({start: null, end: null, selectedDates: {}});
+  const [selectedValue, setSelectedValue] = useState("");
+  const [comment, setComment] = useState('');
+  interface SelectedDates {
+    start: string | null;
+    end: string | null;
+    selectedDates: Record<string, any>;
+  }
+  let newSelectedDates: SelectedDates = {...selectedDates};
+  const { width } = Dimensions.get('window');
+
+  interface Day {
+    dateString: string;
+    day: number;
+    month: number;
+    year: number;
+    timestamp: number;
+  }
+
+  const onDayPress = (day : Day) => {
+    if (newSelectedDates.start == null || (newSelectedDates.start && newSelectedDates.end)) {
+      newSelectedDates = {start: day.dateString, end: null, selectedDates: {[day.dateString]: {selected: true, startingDay: true}}};
+    } else if (!newSelectedDates.end) {
+      newSelectedDates.end = day.dateString;
+      let startDate = new Date(newSelectedDates.start);
+      let endDate = new Date(day.dateString);
+      while (startDate <= endDate) {
+        let dateString = startDate.toISOString().split('T')[0];
+        if (dateString === newSelectedDates.start) {
+          newSelectedDates.selectedDates[dateString] = {selected: true, startingDay: true};
+        } else if (dateString === day.dateString) {
+          newSelectedDates.selectedDates[dateString] = {selected: true, endingDay: true};
+        } else {
+          newSelectedDates.selectedDates[dateString] = {selected: true};
+        }
+        startDate.setDate(startDate.getDate() + 1);
+      }
+    }
+    setSelectedDates(newSelectedDates);
+  };
+  let startDate = selectedDates.start ? new Date(selectedDates.start) : new Date();
+  startDate.setDate(startDate.getDate() + 1);
+  let endDate = selectedDates.end ? new Date(selectedDates.end) : new Date();
+  endDate.setDate(endDate.getDate() + 1);
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View>
+      <Calendar
+        onDayPress={onDayPress}
+        markedDates={selectedDates.selectedDates}
+        style={{
+          width: width, // Set the width to the full width of the device
+          top: 0, // Push it to the top of the screen
+        }}
+      />
+<View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10, width:width }}>
+  <View style={{ alignItems: 'flex-start' }}>
+    <Text style={{fontSize: 25 }}>Start Date</Text>
+    <Text style={{fontSize: 25 }}>{startDate.toLocaleDateString('en-US')}</Text>
+      </View>
+  <View style={{ alignItems: 'flex-end' }}>
+    <Text style={{fontSize: 25 }}>End Date</Text>
+    <Text style={{fontSize: 25 }}>{endDate.toLocaleDateString('en-US')}</Text>
+  </View>
+</View>
+<View>
+<Text style={{ textAlign: 'center', fontSize: 25 }}>Select a Time Off Request Type</Text>
+  <Picker
+    selectedValue={selectedValue}
+    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+    itemStyle={{ color: 'white' }}
+    style={{margin: 0}}
+  >
+    <Picker.Item label="Family Leave" value="familyLeave" />
+    <Picker.Item label="Medical Leave" value="medicalLeave" />
+    <Picker.Item label="Vacation Time" value="vacationTime" />
+    <Picker.Item label="Other" value="other" />
+  </Picker>
+  </View>
+  <View>
+  <Text style={{ textAlign: 'center', fontSize: 25 }}>Comments</Text>
+  <TextInput
+    style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '80%', alignSelf: 'center' }}
+    onChangeText={text => setComment(text)}
+    value={comment}
+    placeholder="Type your comments here..."
+  />
+</View>
       <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', // Center the elements vertically
+    alignItems: 'center', // Center the elements horizontally
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  text: {
+    textAlign: 'center',
+    marginBottom: 10, // Adjust this value to reduce or increase the gap
   },
 });
